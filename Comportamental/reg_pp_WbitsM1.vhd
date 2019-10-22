ca3d4dd7cff8f1c96630fe228ca2939273ebfab8
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2019-10-22 12:55:21 -0300

    Update and rename reg_pp_Wbits.vhd to reg_pp_WbitsM1.vhd

--- Comportamental/reg_pp_WbitsM1.vhd
+++ Comportamental/reg_pp_WbitsM1.vhd
@@ -2,8 +2,9 @@ ENTITY reg_pp_Wbits IS
 GENERIC(W : NATURAL := 4);
 PORT (d : IN BIT_VECTOR(W-1 DOWNTO 0);-- data input
       clk : IN BIT; -- clock
-      clrn: IN BIT; -- clear
-      ena : IN BIT; -- enable/ transforma em 3 bits para poder fazer a comparação na entrada
+      e1 : IN BIT;
+      e2 : IN BIT;
+      e3 : IN BIT;
       q : OUT BIT_VECTOR(W-1 DOWNTO 0));-- data output
 END reg_pp_Wbits;
 
@@ -11,10 +12,8 @@ ARCHITECTURE arch_1 OF reg_pp_Wbits IS
   BEGIN
   PROCESS(clk,clrn)
     BEGIN
-      IF (clrn='0') THEN
-        q <= (OTHERS => '0');
       ELSIF (clk'EVENT AND clk='1') THEN
-        IF (ena='1') THEN
+        IF (e1='0' AND e2='0' AND e3='1') THEN
             q <= d;
         END IF;
       END IF;

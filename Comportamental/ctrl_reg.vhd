33dc750212feed310dc6a5c18cd937c04de53f69
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2019-10-24 08:16:03 -0300

    Update ctrl_reg.vhd

--- Comportamental/ctrl_reg.vhd
+++ Comportamental/ctrl_reg.vhd
@@ -5,16 +5,18 @@ ENTITY ctrl_reg IS
 GENERIC(W : NATURAL := 4);
 PORT (c : IN STD_LOGIC_VECTOR(W-1 DOWNTO 0);-- data input
           r : IN STD_LOGIC_VECTOR(W-1 DOWNTO 0);-- data input
-          saida : OUT BIT;-- data output
+          saida : OUT BIT;
+	clk : IN BIT);-- data output
 END ctrl_reg;
 
 ARCHITECTURE arch_1 OF ctrl_reg IS
     BEGIN
         PROCESS(clk)
         BEGIN
-                ELSIF (clk'EVENT AND clk='1') THEN
+               IF (clk'EVENT AND clk='1') THEN
                     IF (c = r) THEN
                         saida <= '1';
                     END IF;
+	     END IF;
     END PROCESS;
 END arch_1;

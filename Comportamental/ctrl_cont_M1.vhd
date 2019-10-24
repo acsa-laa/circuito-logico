601077d6025b7c663a294c6e5ac5f6c521a7d5ee
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2019-10-24 08:21:04 -0300

    Update ctrl_cont_M1.vhd

--- Comportamental/ctrl_cont_M1.vhd
+++ Comportamental/ctrl_cont_M1.vhd
@@ -4,10 +4,10 @@ USE IEEE.std_logic_unsigned.ALL;
 ENTITY ctrl_cont_M1 IS
 GENERIC(W : NATURAL := 4);
 PORT (c : IN STD_LOGIC_VECTOR(W-1 DOWNTO 0);-- data input
-          e1 : IN OUT;
-          e2 : IN OUT;
-          e3 : IN OUT;
-          e4 : IN OUT;
+          e1 : IN BIT;
+          e2 : IN BIT;
+          e3 : IN BIT;
+          e4 : IN BIT;
           r : OUT STD_LOGIC_VECTOR(W-1 DOWNTO 0);-- data input
           saida : OUT BIT;-- data output
 END ctrl_cont_M1;
